317817d4ce0eb31ac86de868b75e96c2ec177a8c
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-01-20 18:55:44 +0300

    Update and rename errors_1 to error.c

---
+++ error.c
@@ -0,0 +1,12 @@
+int u#include "monty.h"
+/**
+  * arg_err - printss error for incomplete program usage argument
+  *
+  * Return: void
+  */
+void arg_err(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+}sage_error(void);
+int usage_error(void);
